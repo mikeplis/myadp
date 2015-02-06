c0253b2cd3d10575ca51be28a01aaf55683ddd01
Mike Plis <mplis@novus.com>
2015-02-06 11:59:14 -0500

    updated view

--- hello/views.py
+++ hello/views.py
@@ -6,9 +6,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse('Hello world')
 
 
 def db(request):
