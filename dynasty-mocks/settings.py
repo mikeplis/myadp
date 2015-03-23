0d0a7cf4480d047024ada839a65a065b41f34e02
mplis <michael.plis@huffingtonpost.com>
2015-03-23 12:50:44 -0400

    fix settings

--- dynasty-mocks/settings.py
+++ dynasty-mocks/settings.py
@@ -102,9 +102,9 @@ else:
 #STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 
